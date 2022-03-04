03d1f2e1817122bfba374a59f476d56139a5bebb
Sergio Ordoñez <sergio.ordonez.co@gmail.com>
2022-03-04 12:11:22 -0500

    first commit

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,30 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository: IPostRepository
+    {
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Descrition {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            
+            });
+
+            await Task.Delay(10);
+
+            return posts;
+        }
+    }
+}
